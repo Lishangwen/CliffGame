0d7adef651af4a4acbf49c7b3a7958640d45e5c0
Lishangwen <lswscu@foxmail.com>
2018-09-03 17:15:20 +0800

    加入登陆界面
    加入简单登陆界面，修改工程目录结构

--- Content/Lua/TestUI.lua
+++ Content/Lua/TestUI.lua
@@ -6,20 +6,22 @@ TestUI= TestUI or
 function TestUI.text()
 end
 
-local ui=slua.loadUI('/Game/Panel.Panel');
+local ui=slua.loadUI('/Game/UMG/Panel.Panel');
 ui:AddToViewport(0);
 local btn2=ui:FindWidget('Button1');
 
 function TestUI.AA()
     print('Button 2 clicked!!!!');
+    --ui:RemoveFromParent();
 end
 
 btn2.OnClicked:Add(TestUI.AA);            -- 绑定方式1：函数绑定，但是函数需要先定义再绑定
 
+--[[
 btn2.OnClicked:Add(function ()            -- 绑定方式2：闭包函数绑定
-    print('Button 2 clicked!!!!');
+    print('Button 1 clicked!!!!');
 end)
-
+]]
 function update(dt,actor)
    
 end
